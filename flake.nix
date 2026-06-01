30df33fdf8b81b4bf9f64e16683d2a6f3717ccf2
6543 <6543@obermui.de>
2026-06-02 00:05:19 +0200

    Add flake.nix to compile and develop

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      crane,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        lib = nixpkgs.lib;
+        craneLib = crane.mkLib pkgs;
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.;
+        };
+
+        commonArgs = { inherit src; };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        bin = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+      in
+      with pkgs;
+      {
+        packages = {
+          default = bin;
+        };
+
+        devShells.default = mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+            rust-analyzer
+          ];
+        };
+
+        formatter = nixpkgs-fmt;
+      }
+    );
+}
